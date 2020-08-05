922d32f753d44db3e3dea517c7b7f41977fe8126
ywenli <kuota0204@gmail.com>
2020-08-05 12:22:45 +0900

    Add registrations_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations'}
+
   root 'pages#home'
 
   get '/users/:id', to: 'users#show', as: 'user'
