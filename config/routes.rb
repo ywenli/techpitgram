4fc7a052044c91b5f33e64030a45d534ce954143
ywenli <kuota0204@gmail.com>
2020-08-05 13:28:36 +0900

    Add routing related to post

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   get '/users/:id', to: 'users#show', as: 'user'
 
+  get '/posts/new', to: 'posts#new'
+  post '/posts', to: 'posts#create'
+  post '/posts/:post_id/photos', to: 'photos#create', as: 'post_photos'
+
 end
