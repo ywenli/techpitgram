73d48fcdc34d54653ceeea6f5a3a618d4de762c5
ywenli <kuota0204@gmail.com>
2020-08-05 23:55:57 +0900

    Add link to undo likes and likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,9 @@ class Post < ApplicationRecord
 
   accepts_nested_attributes_for :photos
 
+  def liked_by(user)
+    # user_idとpost_idが一致するlikeを検索する
+    Like.find_by(user_id: user.id, post_id: id)
+  end
+
 end
