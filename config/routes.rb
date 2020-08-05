c685d0bf72f5dbb49ee9ede4f07966c0731cdf15
ywenli <kuota0204@gmail.com>
2020-08-05 18:44:42 +0900

    Add routing related to like

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
+    resources :likes, only: %i(create destroy)
   end
 end
