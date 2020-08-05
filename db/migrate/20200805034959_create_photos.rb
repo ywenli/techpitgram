43421cbd245948530be15b08351c89c9643bca45
ywenli <kuota0204@gmail.com>
2020-08-05 12:59:22 +0900

    Update migration file

--- db/migrate/20200805034959_create_photos.rb
+++ db/migrate/20200805034959_create_photos.rb
@@ -1,7 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-
+      t.string :image, null: false
+      t.references :post, foreign_key: true, null: false
       t.timestamps
     end
   end
