15a8f734836e603aa7751c9158305a898ae42f25
ywenli <kuota0204@gmail.com>
2020-08-05 12:49:21 +0900

    Delete duplicate migration file

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_08_05_033427) do
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "caption"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -34,5 +26,4 @@ ActiveRecord::Schema.define(version: 2020_08_05_033427) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "posts", "users"
 end
