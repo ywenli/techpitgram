bc2d3e0d78512c69f2c3f8b40604c623e1a1f77e
ywenli <kuota0204@gmail.com>
2020-08-05 18:53:42 +0900

    Add action: create

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+
+  def create
+    @like = current_user.likes.build(like_params)
+    @post = @like.post
+    if @like.save
+      respond_to :js
+    end
+  end
+
+  private
+
+    def like_params
+      params.permit(:post_id)
+    end
+
 end
