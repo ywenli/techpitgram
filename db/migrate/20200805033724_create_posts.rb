bc071df3896ab51e147d792ba1fc1ae9631c0ab5
ywenli <kuota0204@gmail.com>
2020-08-05 12:38:25 +0900

    Destroy and Generate Post model

--- db/migrate/20200805033724_create_posts.rb
+++ db/migrate/20200805033724_create_posts.rb
@@ -1,8 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :caption
-      t.references :user, foreign_key: true, null: false
+
       t.timestamps
     end
   end
