4a360016eff17446ff6ef07e37b1e176b8d8cf9b
ywenli <kuota0204@gmail.com>
2020-08-05 12:35:53 +0900

    Update migration file

--- db/migrate/20200805033427_create_posts.rb
+++ db/migrate/20200805033427_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string :caption
+      t.references :user, foreign_key: true, null: false
       t.timestamps
     end
   end
