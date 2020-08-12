276458b4151f16b35002a366d544b19cdf77f1fc
ywenli <kuota0204@gmail.com>
2020-08-12 10:05:15 +0900

    Update migration file #4

--- db/migrate/20200812010018_add_username_to_user.rb
+++ db/migrate/20200812010018_add_username_to_user.rb
@@ -1,5 +1,5 @@
 class AddUsernameToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :username, :string
+    add_column :users, :username, :string, null: false, default: ""
   end
 end
